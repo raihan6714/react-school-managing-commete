8c8f7c5c48c649a44a87df4699ed574dbf2f2668
raihan400 <ab40096@gmail.com>
2021-09-26 00:06:35 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <nav className="bg-light">
+                <div className="header-title">
+                    <h1 className="">Make a School Managing Committee</h1>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
